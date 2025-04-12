3b70df5a194995c0ed059de211469fd26bd2b80c
Juan Carlos Torrico <samieltx@gmail.com>
2025-04-11 23:32:49 -0400

    add Clase File

---
+++ data_load/read_images.py
@@ -0,0 +1,36 @@
+class File:
+    
+    def __init__(self):
+        """Constructor de la clase File, inicializa el diccionario images
+        """
+        self.images = {}
+    
+    def _data_load(self):
+        """Funcion de cargado de datos iniciales sobre las imagenes
+            Genera la metadata de los archivos y los guarda en un diccionario de datos        
+        """
+        import os
+        
+        os.chdir('../covid/images')
+        curr_dir = os.getcwd()
+        
+        for file in os.listdir(curr_dir):
+            file_name = os.path.splitext(file)
+            lista = file_name[0].split("-")
+            id = lista[1]
+            extension = file_name[1][1:]            
+            ruta = os.path.join(curr_dir,file)            
+            tamaño = os.path.getsize(ruta)
+            self.images[id] = {"id": id,"filename":file,"extension":extension,"path":ruta,"size":tamaño}            
+    
+    def show_data(self):
+        """Funcion que muestra los datos del diccionario relaciondos a imagenes
+        """
+        for key,valor in self.images.items():
+            print("id:",valor['id']," filename:", valor['filename'], " extension:", valor['extension'], " path", valor['path'], " size:", valor['size'])            
+            
+            
+if __name__ == "__main__":
+    file = File()
+    file._data_load()
+    file.show_data()
