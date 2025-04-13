7cf6038ca2e0fd8d6d4d054c924381debfa70ee9
Juan Carlos Torrico <jctorricor@soe.uagrm.edu.bo>
2025-04-13 17:27:54 -0400

    correccion init.py

--- init.py
+++ init.py
@@ -1,27 +1,44 @@
+from data_load.read_images import File
+from crud.agregar import ProcessImage
 from crud.modificar import renombrar_archivo
 
-
+file = File()
+file._data_load()
+#file.show_data()
+    
 while True:
-    print("-" * 50)
-    print("Gestionar imagenes de COVID-19 Radiography Database:")
+    print("-" * 54)
+    print("\nGestionar imagenes de COVID-19 Radiography Database:\n")
     print("       1: Agregar nueva imagen")
     print("       2: Modificar imagen")
     print("       3: Eliminar imagen")
-    print("       0: Salir")
-    print("-" * 50)
+    print("       4: Mostrar imagenes cargadas")
+    print("       0: Salir\n")
+    print("-" * 54)
 
-    opcion = int(input("Opcion: "))
-
-    if opcion == 1:
-        print("Seleecionaste Opcion 1")
-    elif opcion == 2:
-        print("Seleecionaste Opcion 2")
-        renombrar_archivo()
-    elif opcion == 3:
-        print("Seleecionaste Opcion 3")
-    elif opcion == 0:
-        print("Seleecionaste Opcion 0")
-        break
-    else:       
-        print("Opcion no valida seleccione una opcion de la lista")
-                
+    try:
+        opcion = int(input("Opcion: "))
+    except Exception as e:
+        print("ERROR: ", e)
+        
+        if opcion == 1:
+            p = ProcessImage()
+            pathfile = p.copiar() 
+            print(pathfile)
+            procesado = file._data_process(pathfile)
+            
+            if procesado:      
+                file._del_file_procesado(pathfile)            
+        
+        elif opcion == 2:
+            print("Selecionaste Opcion 2")
+            renombrar_archivo()
+             
+        elif opcion == 3:
+            print("Selecionaste Opcion 3")
+        elif opcion == 4:
+            file.show_data()
+        elif opcion == 0:
+            print("Selecionaste Opcion 0")
+            break
+            
